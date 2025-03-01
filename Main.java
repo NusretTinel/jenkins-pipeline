bbb6ed01d9cf69d5faaacea45c2e7aaf3ebb2982
NusretTinel <117197831+NusretTinel@users.noreply.github.com>
2025-03-01 18:03:48 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-// Main.java
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Jenkins!");
+        // Konteynerin kapanmasını önlemek için sonsuz döngü
+        while (true) {
+            Thread.sleep(9000); // 1 saniye bekle
+        }
     }
 }
