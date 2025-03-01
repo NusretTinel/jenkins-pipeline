910f4f7d3de05f000fd9619b49ea56c503a651c5
NusretTinel <117197831+NusretTinel@users.noreply.github.com>
2025-03-01 18:01:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,6 @@
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
+// Main.java
 public class Main {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", exchange -> {
-            String response = "Jenkins’tan Merhaba!";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
-        server.setExecutor(null); // Varsayılan executor
-        server.start();
-        System.out.println("Server 8080 portunda çalışıyor...");
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
     }
 }
