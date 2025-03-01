0478f5e74f830defb59a7c4c14879e067d166cc3
NusretTinel <117197831+NusretTinel@users.noreply.github.com>
2025-03-01 17:55:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
-// Main.java
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Jenkins’tan Merhaba!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Jenkins’tan Merhaba!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.setExecutor(null); // Varsayılan executor
+        server.start();
+        System.out.println("Server 8080 portunda çalışıyor...");
     }
 }
